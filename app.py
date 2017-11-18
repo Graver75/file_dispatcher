d490f10963913fba1c0603773a2a61c78094fb64
Ilya <undergraver75@gmail.com>
2017-11-18 18:19:11 +0300

    number of port now from cli

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import sys
 from twisted.internet import endpoints, reactor
 
 from p2p_factory import P2PFactory
 from p2p_protocol import P2PProtocol
 
 # server
-endpoint = endpoints.TCP4ServerEndpoint(reactor, 7003)
+endpoint = endpoints.TCP4ServerEndpoint(reactor, int(sys.argv[1]))
 endpoint.listen(P2PFactory())
 
 # client
-point = endpoints.TCP4ClientEndpoint(reactor, "localhost", 7003)
+point = endpoints.TCP4ClientEndpoint(reactor, "localhost", int(sys.argv[1]))
 d = endpoints.connectProtocol(point, P2PProtocol(factory=P2PFactory))
 reactor.run()
