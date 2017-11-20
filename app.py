fee01b06a81746afc3d9a78b79de2e24b3318ad5
Ilya <undergraver75@gmail.com>
2017-11-20 22:49:03 +0300

    bug fixes, nodes now shares with each other peer info

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ endpoint.listen(factory)
 # client
 for port in BOOTSTRAP_LIST:
     point = endpoints.TCP4ClientEndpoint(reactor, "localhost", int(port))
-    d = endpoints.connectProtocol(point, P2PProtocol(factory=factory))
+    d = endpoints.connectProtocol(point, P2PProtocol(factory=factory, peer_type=1))
 reactor.run()
--- p2p_protocol.py
+++ p2p_protocol.py
@@ -1,23 +1,35 @@
 from twisted.internet.protocol import Protocol
 from twisted.internet.task import LoopingCall
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet import reactor
 from time import time
 
 import json
 
 RECOVERY_DELAY = 120
-PING_INTERVAL = 60
+PING_INTERVAL = min(RECOVERY_DELAY, 60)
 
 
 class P2PProtocol(Protocol):
-    def __init__(self, factory):
+    """
+    Peer type 0 means that we have a connection from him
+    Peer type 1 means that we try to connect to him
+    """
+
+    def __init__(self, factory, peer_type=0):
         self.factory = factory
         self.state = "HELLO"
         self.remote_node_id = None
         self.lc_ping = LoopingCall(self.send_ping)
         self.last_ping = 0
+        self.peer_type = peer_type
 
     def connectionMade(self):
         print('Connection made from', self.transport.getPeer())
+        remote_ip = self.transport.getPeer()
+        host_ip = self.transport.getHost()
+        self.remote_ip = remote_ip.host + ":" + str(remote_ip.port)
+        self.host_ip = host_ip.host + ":" + str(host_ip.port)
         self.last_ping = time()
         self.send_hello()
 
@@ -26,7 +38,7 @@ class P2PProtocol(Protocol):
         if self.remote_node_id in self.factory.peers:
             self.factory.peers.pop(self.remote_node_id)
             self.lc_ping.stop()
-        print(self.remote_node_id, 'disconnected')
+        print(self.remote_node_id, ': disconnected')
 
     def dataReceived(self, data):
         for line in data.splitlines():
@@ -39,6 +51,18 @@ class P2PProtocol(Protocol):
                 self.handle_ping()
             elif msg_type == "pong":
                 self.handle_pong()
+            elif msg_type == "getaddr":
+                self.handle_getaddr()
+            elif msg_type == "addr":
+                self.handle_addr(line)
+
+    def handle_addr(self, addr):
+        addr = json.loads(addr)
+        for remote_ip, remote_node_id in addr['peers']:
+            if remote_node_id not in self.factory.peers:
+                host, port = remote_ip.split(":")
+                point = TCP4ClientEndpoint(reactor, host, int(port))
+                d = connectProtocol(point, P2PProtocol(factory=self.factory))
 
     def handle_hello(self, hello):
         hello = json.loads(hello)
@@ -47,18 +71,20 @@ class P2PProtocol(Protocol):
             print('Connection loop')
             self.transport.loseConnection()
         else:
-            print(self.remote_node_id, 'connected')
-            self.factory.peers[self.remote_node_id] = self.transport.getPeer()
+            print(self.remote_node_id, ': hello')
+            self.factory.peers[self.remote_node_id] = self
             self.lc_ping.start(PING_INTERVAL)
+            self.send_addr(mine=True)
+            self.send_getaddr()
 
     def send_hello(self):
-        hello = json.dumps({'node_id': self.factory.node_id, 'msgtype': 'HELLO'})
+        hello = json.dumps({'node_id': self.factory.node_id, 'msgtype': 'hello'})
         self.transport.write(bytes(hello + '\n', 'utf8'))
 
     def send_ping(self):
-        if not self.is_dead_node():
+        if not self.is_dead_node(self):
             ping = json.dumps({'msgtype': 'ping'})
-            print("Pinging", self.remote_node_id)
+            print(self.remote_node_id, ": ping")
             self.transport.write(bytes(ping + '\n', 'utf8'))
         else:
             self.transport.loseConnection()
@@ -71,8 +97,27 @@ class P2PProtocol(Protocol):
         self.send_pong()
 
     def handle_pong(self):
-        print("Got pong from", self.remote_node_id)
+        print(self.remote_node_id, ": pong")
         self.last_ping = time()
 
-    def is_dead_node(self):
-        return (time() - self.last_ping) > RECOVERY_DELAY
+    @staticmethod
+    def is_dead_node(node):
+        return (time() - node.last_ping) > RECOVERY_DELAY
+
+    def handle_getaddr(self):
+        self.send_addr()
+
+    def send_addr(self, mine=False):
+        if mine:
+            peers = [(self.host_ip, self.factory.node_id)]
+        else:
+            peers = [(self.factory.peers[peer].remote_ip, self.factory.peers[peer].remote_node_id)
+                     for peer in self.factory.peers
+                     if self.factory.peers[peer].peer_type == 0 and
+                     not self.is_dead_node(self.factory.peers[peer])]
+        addr = json.dumps({"msgtype": "addr", "peers": peers})
+        self.transport.write(bytes(addr + '\n', 'utf8'))
+
+    def send_getaddr(self):
+        request = json.dumps({"msgtype": "getaddr"})
+        self.transport.write(bytes(request + '\n', 'utf8'))
