8026980387b0899ab7deddc22a3ccac577243806
Илья <undergraver75@gmail.com>
2017-12-07 01:23:10 +0300

    import to if

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from twisted.cred.checkers import AllowAnonymousAccess, FilePasswordDB
 
 from p2p_factory import P2PFactory
 from p2p_protocol import P2PProtocol
-from http_control_server import HTTPControlServer
 
 PORT = int(sys.argv[1])
 WANNA_CONTROL_SERVER = bool(int(sys.argv[2]))
@@ -31,5 +30,6 @@ for address in BOOTSTRAP_LIST:
     point = endpoints.TCP4ClientEndpoint(reactor, host, int(port))
     d = endpoints.connectProtocol(point, P2PProtocol(factory=factory, peer_type=1))
 if WANNA_CONTROL_SERVER:
+    from http_control_server import HTTPControlServer
     HTTPControlServer(factory).app.run('localhost', 8080)
 reactor.run()
